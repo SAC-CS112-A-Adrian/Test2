557b69a29dbfb9ca844ae31f240ccfd090e07aeb
SAC-CS112-A-Adrian <pw2422@gmail.com>
2016-05-04 22:05:46 -0700

    Added files via upload

---
+++ Dice.java
@@ -0,0 +1,19 @@
+
+public class Dice {
+	
+	private int maximum;
+	private int diceRolled;
+	
+	public Dice ()
+	{
+		maximum = 6;
+	}
+	
+	public int rollDice (int number)
+	{
+		int max = maximum * number;
+		diceRolled = ( number + (int) (Math.random() * max) );
+		return diceRolled;
+	}
+
+}
